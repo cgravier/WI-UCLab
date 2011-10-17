ffd2e10b9a616b7bbc80b6a647cc57021bba93a0
Christophe Gravier <christophe.gravier@telecom-st-etienne.fr>
2011-10-17 15:16:41 +0200

    edit server README to correct shutters open/close http command
    Signed-off-by: Christophe Gravier <christophe.gravier@telecom-st-etienne.fr>

--- context-server-nodejs/launch15nodes.sh
+++ context-server-nodejs/launch15nodes.sh
@@ -1,21 +1,26 @@
-node simulateArduino.js 127.0.0.1 8001 &
-node simulateArduino.js 127.0.0.1 8002 &
-node simulateArduino.js 127.0.0.1 8003 &
-node simulateArduino.js 127.0.0.1 8004 &
-node simulateArduino.js 127.0.0.1 8005 &
-node simulateArduino.js 127.0.0.1 8006 &
-node simulateArduino.js 127.0.0.1 8007 &
-node simulateArduino.js 127.0.0.1 8008 &
-node simulateArduino.js 127.0.0.1 8009 &
-node simulateArduino.js 127.0.0.1 8010 &
-node simulateArduino.js 127.0.0.1 8011 &
-node simulateArduino.js 127.0.0.1 8012 &
-node simulateArduino.js 127.0.0.1 8013 &
-node simulateArduino.js 127.0.0.1 8014 &
-node simulateArduino.js 127.0.0.1 8015 &
-node simulateArduino.js 127.0.0.1 8016 &
-node simulateArduino.js 127.0.0.1 8017 &
-node simulateArduino.js 127.0.0.1 8018 &
-node simulateArduino.js 127.0.0.1 8019 &
-node simulateArduino.js 127.0.0.1 8020 &
+HOST=161.3.117.110
+
+# beware if you have other node services running :-)
+killall node
+
+node simulateArduino.js $HOST 8001 &
+node simulateArduino.js $HOST 8002 &
+node simulateArduino.js $HOST 8003 &
+node simulateArduino.js $HOST 8004 &
+node simulateArduino.js $HOST 8005 &
+node simulateArduino.js $HOST 8006 &
+node simulateArduino.js $HOST 8007 &
+node simulateArduino.js $HOST 8008 &
+node simulateArduino.js $HOST 8009 &
+node simulateArduino.js $HOST 8010 &
+node simulateArduino.js $HOST 8011 &
+node simulateArduino.js $HOST 8012 &
+node simulateArduino.js $HOST 8013 &
+node simulateArduino.js $HOST 8014 &
+node simulateArduino.js $HOST 8015 &
+node simulateArduino.js $HOST 8016 &
+node simulateArduino.js $HOST 8017 &
+node simulateArduino.js $HOST 8018 &
+node simulateArduino.js $HOST 8019 &
+node simulateArduino.js $HOST 8020 &
 
